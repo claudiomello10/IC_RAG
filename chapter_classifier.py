fd23fb531fc28100b3463dd7e0c386142c30b215
Rebecca Aben-Athar <rebecca.athar@itec.ufpa.br>
2024-10-29 21:19:19 -0300

    feat: dividing the classification into main chapter and subchapter

--- chapter_classifier.py
+++ chapter_classifier.py
@@ -1,52 +1,68 @@
 import fitz  # PyMuPDF
 from transformers import pipeline
-# Step 2: Define the path to the PDF file
-#PATH = "Livros/Futuros/MachineLearning/2024_Python_Deep_Learning_w_pacc21.pdf" # -- bom
-#PATH = "Livros/Futuros/MachineLearning/2019_Book_EmbeddedDeepLearning.pdf"
-#PATH = "Livros/Futuros/MachineLearning/2018_Book_DeepLearningWithAzure.pdf"
-#PATH = "Livros/Futuros/MachineLearning/2018_Book_DeepLearningWithApplicationsUsingPython.pdf"
-#PATH = "Livros/Futuros/MachineLearning/2017_Book_DeepLearningWithPython.pdf"
-PATH = "Livros/Futuros/MachineLearning/2017_Book_ProDeepLearningWithTensorFlow.pdf"
-#PATH = "Livros/Futuros/MachineLearning/'2022_Aurélien Géron - Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow_ Concepts, Tools, and Techniques to Build Intelligent Systems-O'\''Reilly Media (2022).pdf'"
-#PATH = "Livros/Futuros/MachineLearning/Adrian Rosebrock-Deep Learning for Computer Vision with Python. 3-ImageNetBundle-PyImageSearch (2017).pdf'"
 
-# Step 3: Open the PDF file
+# Step 1: Choose a PDF file
+
+#PATH = "Livros/Futuros/MachineLearning/2024_Python_Deep_Learning_w_pacc21.pdf" # -- bom
+#PATH = "Livros/Futuros/MachineLearning/2019_Book_EmbeddedDeepLearning.pdf" #bom
+#PATH = "Livros/Futuros/MachineLearning/2018_Book_DeepLearningWithAzure.pdf" # hmm mutchu bom
+PATH = "Livros/Futuros/MachineLearning/2018_Book_DeepLearningWithApplicationsUsingPython.pdf"# very good rebecca
+#PATH = "Livros/Futuros/MachineLearning/2017_Book_DeepLearningWithPython.pdf" #hmmm thats right
+#PATH = "Livros/Futuros/MachineLearning/2017_Book_ProDeepLearningWithTensorFlow.pdf" # oh yeah
+#PATH = "/home/rebecca/IC_RAG/Livros/Futuros/MachineLearning/Adrian Rosebrock-Deep Learning for Computer Vision with Python. 2-Practitioner Bundle-PyImageSearch (2017).pdf" #well done!
+#PATH = "Livros/Futuros/MachineLearning/2022_Aurélien Géron - Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow_ Concepts, Tools, and Techniques to Build Intelligent Systems-O'Reilly Media (2022).pdf"
+#PATH = "Livros/Futuros/MachineLearning/François Chollet-Deep Learning with Python-Manning (2018).pdf"
+#PATH = "Livros/Futuros/MachineLearning/Sebastian Raschka-Python Machine Learning-Packt Publishing (2015).pdf"
+#PATH = "Livros/Futuros/MachineLearning/daumé-imitation-learning.pdf"
+#PATH = "Livros/Futuros/MachineLearning/2015_machine_learning_a_bayesian_perspective.pdf"
+#PATH = "Livros/Futuros/MachineLearning/2018_Aggarwal_NeuralNetworksAndDeepLearning.pdf"
+#PATH = "Livros/Futuros/MachineLearning/2022_Chuo_Convex Optimization for Machine Learning.pdf"
+#PATH = "Livros/Futuros/MachineLearning/Adrian Rosebrock-Deep Learning for Computer Vision with Python. 2-Practitioner Bundle-PyImageSearch (2017).pdf" #well done!
+#PATH = "Livros/Futuros/MachineLearning/Adrian Rosebrock-Deep Learning for Computer Vision with Python. 3-ImageNetBundle-PyImageSearch (2017).pdf"
+#PATH = "Livros/Futuros/MachineLearning/Adrian Rosebrock-Deep Learning for Computer Vision with Python. 1,Starter Bundle-PyImageSearch (2017).pdf"
+
+# Step 2: Open the PDF file
 doc = fitz.open(PATH)
 
-# Step 4: Extract the bookmarks (table of contents)
+# Step 3: Extract the bookmarks (table of contents) and divide into "main section" and "subsection"
 toc = doc.get_toc()
 
-section = []
+main_section = []
+sub_section = []
+
 # Step 5: Print the bookmarks
 for i in range(len(toc)):
-    if(toc[i][0] == 1):
-        section.append(toc[i]) 
+    if(toc[i][0] == 1 ):
+        main_section.append(toc[i]) 
         print(toc[i])   
+    if(toc[i][0] == 2 ):
+        sub_section.append(toc[i]) 
 
+candidate_labels1 = [ "Main chapter", "Appendix", "Index",  "Acronyms Section", "Preface", "Acknowledgments",  "References" ]
+candidate_labels2 = [ "Main chapter", "index", "references" , "summary"]
 
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-candidate_labels = ["Key Chapter Content", "appendix", "Index", "Acronyms" "Preface", "Acknowledgments", "A topic related to the Key Chapter Content", "Preliminary Section"]
+hypothesis_template = "This text title is a {} section of the book."
 
-hypothesis_template = "This title represents which type of section in the book? {}"
-
-'''
-for item in section:
-    sequence_to_classify = f"Title: '{item[1]}'"
-    
-    result = classifier(sequence_to_classify, candidate_labels)
-    
-    if result['labels'][0] == "Key Chapter Content" and result['scores'][0] > 0.20:
-        print(f"Key Chapter Content: {item[1]} (page {item[2]})  score: {result['scores'][0]:.2f}")
-'''
 
 zeroshot_classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0")
+first = []
 
-
-for title in section:
+for title in main_section:
     strings = [item for item in title if isinstance(item, str)]
-    output = zeroshot_classifier(strings, candidate_labels, hypothesis_template=hypothesis_template, multi_label=False)
+    output = zeroshot_classifier(strings, candidate_labels1, hypothesis_template=hypothesis_template, multi_label=False)
     output = output[0]
     for label, score in zip(output['labels'], output['scores']):
-        if label == "Key Chapter Content" and score > 0.20: 
-            print(f' Título: {strings},  Classe: {label}, Score: {score:.2f}')  
+        if label == "Main chapter" and score > 0.30: 
+            print(f' Main chapter: {strings},  Class: {label}, Score: {score:.2f}')  
+
+
+print("--------subsection---------------")
+for title in sub_section:
+    strings = [item for item in title if isinstance(item, str)]
+    output = zeroshot_classifier(strings, candidate_labels2, hypothesis_template=hypothesis_template, multi_label=False)
+    output = output[0]
+    for label, score in zip(output['labels'], output['scores']):
+        if label == "Main chapter" and score > 0.10: 
+            print(f' Subtitle: {strings},  Class: {label}, Score: {score:.2f}')  
+
 
